f6bdf7a3c0679f7dc8f0b1af3c4b7902a72f66f3
edx <edx1010@gmail.com>
2011-01-02 16:59:03 +0100

    Support for a PHP based SH

--- modules/syntaxhighlighter/sh.js
+++ modules/syntaxhighlighter/sh.js
@@ -2,40 +2,16 @@ this.functionList = new Array();
 this.name = null;
 
 var object;
-var style = "modules/syntaxhighlighter/styles/shEdx.css";
-
-var languages = new Array(
-		new Array("bash", "modules/syntaxhighlighter/languages/shBrushBash.js"),
-		new Array("c", "modules/syntaxhighlighter/languages/shBrushCpp.js"),
-		new Array("cpp", "modules/syntaxhighlighter/languages/shBrushCpp.js"),
-		new Array("perl", "modules/syntaxhighlighter/languages/shBrushPerl.js")
-);
 
 this.init = function(obj)
 {
 	object = obj;
-	addScript(netSH_prefix + "modules/syntaxhighlighter/shCore.js");
-	addStyle(netSH_prefix + "modules/syntaxhighlighter/styles/shCore.css");
-	addStyle(netSH_prefix + style);
-
-	for(var i = 0; i < languages.length; i++)
-		addScript(netSH_prefix + languages[i][1]);
+	addStyle(netSH_prefix + "modules/syntaxhighlighter/styles/sh.css");
 }
 
-this.highlight = function(lang)
+this.highlight = function(file, lang)
 {
-	var current_module = null;
-	var ready = false; 
-
-	for(var i = 0; i < languages.length; i++)
-		if(languages[i][0] == lang)
-		{
-			var elem = document.getElementById("toHighlight");
-			SyntaxHighlighter.highlight(elem);
-			elem = document.getElementById("toHighlight");
-			elem.setAttribute("id", "");
-		}
-
+	write("<pre class=sh>" + loadFILE(netSH_prefix + 'modules/syntaxhighlighter/sh.php?file=' + file + '&lang=' + lang) + "</pre>");
 	return;
 }
 
@@ -44,22 +20,12 @@ function loadFILE(file)
 	return eval(object + '.loadFILE_pub("' + file + '");');
 }
 
-function loadModule(name, module)
+function write(text)
 {
-	eval(object + '.addModule_pub(name, module);');
-}
-
-function getModuleByName(name)
-{
-	return eval(object + '.getModuleByName("' + name + '");');
+	eval(object + '.write(text);');
 }
 
 function addStyle(src)
 {
-	return eval(object + '.addStyle_pub(src);');
-}
-
-function addScript(src)
-{
-	return eval(object + '.addScript_pub(src);');
+	eval(object + '.addStyle_pub(src);');
 }
